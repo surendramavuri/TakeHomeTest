b242729ddfe93f133e9d6674d90045ec3e302ce5
Sri Satish M <surendramavuri@gmail.com>
2025-03-11 01:05:21 -0500

    Initial Commit

---
+++ TakeHomeTest/TakeHomeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TakeHomeTestApp.swift
+//  TakeHomeTest
+//
+//  Created by Sri Satish M on 3/11/25.
+//
+
+import SwiftUI
+
+@main
+struct TakeHomeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
